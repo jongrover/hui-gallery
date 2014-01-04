77dc8f90ad2f3afea40c9ca5825f227fab152632
jongrover <hello@jonathangrover.com>
2014-01-03 23:06:47 -0500

    add fancybox

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ $(function(){
   //navigation
 
   $('section').hide(); //intially hide all sections
+  $('#illustration').show(); //show illustration on start
   $('nav').find('a').click(function(){
     var selected = $(this).attr('href');
     $('section').hide(); //hide all sections
     $(selected).show(); //show the selected section
   });
 
-  //
+  //fancybox
+  $("#gallery").find('a').fancybox({
+    prevEffect  : 'none',
+    nextEffect  : 'none',
+    helpers : {
+      title : {
+        type: 'outside'
+      },
+      thumbs  : {
+        width : 50,
+        height  : 50
+      }
+    }
+  });
 
 });

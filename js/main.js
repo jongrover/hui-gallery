13edf9cf65ec2ba6bca06943ea15de994dfe1476
jongrover <hello@jonathangrover.com>
2014-01-05 22:54:54 -0500

    finish sub_nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 $(function(){
 
-  //navigation
+  ////////// $GLOBALS //////////
+
+  var $main_nav = $('header').find('nav').find('a'),
+      $sub_nav = $('#gallery').find('section').find('nav').find('a'),
+      $fancy_img = $("#gallery").find('section').find('div').find('a'),
+      $show_first = $('#illustration');
+
+  ////////// NAVIGATION //////////
 
   $('section').hide(); //intially hide all sections
-  $('#illustration').show(); //show illustration on start
-  $('nav').find('a').click(function(){
+  $show_first.show().find('div').hide().first().show(); //show illustration on start
+
+  $main_nav.click(function(e){
     var selected = $(this).attr('href');
     $('section').hide(); //hide all sections
     $(selected).show(); //show the selected section
+    $(selected).find('div').hide(); // hide divs within newly shown section
+    $(selected).find('div').first().show(); //show first div in the section
   });
 
-  //fancybox
-  $("#gallery").find('a').fancybox({
+  $sub_nav.click(function(){
+    var selected = $(this).attr('href');
+    console.log(selected);
+    $(this).parents('section').find('div').hide(); // hide divs within this section
+    $(selected).show(); //show first div in the section
+  });
+
+  ////////// FANCY BOX GALLERIES //////////
+
+  $fancy_img.fancybox({
     prevEffect  : 'none',
     nextEffect  : 'none',
     helpers : {
